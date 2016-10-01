0f19da1820247b48fe3e5ea42ed0488284ddc740
DarkKnight6499 <yazad.madan@gmail.com>
2016-10-01 22:47:42 +0530

    First commit3

---
+++ q1010i-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xolo/q1010i/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xolo/q1010i/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xolo/q1010i/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xolo/q1010i/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xolo/q1010i/xbin,system/xbin)
+
